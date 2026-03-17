bd9aba3c2000f0214903bb752c4e666c618da2ca
Christian Olsen <christian@cjsolsen.com>
2026-03-17 17:19:58 +0100

    chore: update apple defaults
    Signed-off-by: Christian Olsen <christian@cjsolsen.com>

--- install.sh
+++ install.sh
@@ -99,32 +99,97 @@ mas install 6752347588 #Spool buddy
 # Set macOS settings
 echo "Changing macOS defaults..."
 
+# show file extensions
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-defaults write NSGlobalDomain AppleHighlightColor -string "0.79608 0.65098 0.96863"
+# set highligt color
+defaults write NSGlobalDomain AppleHighlightColor -string "0.85 0.85 0.85"
+# set accent color to purple
 defaults write NSGlobalDomain AppleAccentColor -int 5
+# save screenshots to downloads folder
 defaults write com.apple.screencapture location -string "$HOME/Downloads"
+# show screenshot thumbnail
+defaults write com.apple.screencapture show-thumbnail -bool false
+# set screenshot format
+defaults write com.apple.screencapture type -string png
+# trun off window opening animations
 defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+# prefer to save files locally
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+# set sidebar icons size in Finder
+defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
+# enable tab to move focus
+defaults write NSGlobalDomain AppleKeyboardUIMode -int "2"
+# switch to space with open window
+defaults write NSGlobalDomain AppleSpacesSwitchOnActivate -bool true
+# don't ask to use external disk for backups
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+# don't show downloaded warning
+defaults write com.apple.LaunchServices LSQuarantine -bool false
 
+# make dock auto hide
 defaults write com.apple.dock autohide -bool true
-defaults write com.apple.dock autohide-time-modifier -float 0.001
-defaults write com.apple.dock springboard-show-duration -float 0.001
-defaults write com.apple.dock springboard-hide-duration -float 0.001
-defaults write com.apple.dock springboard-page-duration -float 0.001
+# set dock location to left
+defaults write com.apple.dock orientation left
+# set icon size
+defaults write com.apple.dock tilesize -int 48
+# set animation times (fast as possible)
+defaults write com.apple.dock autohide-time-modifier -float 0.0
+defaults write com.apple.dock springboard-show-duration -float 0.0
+defaults write com.apple.dock springboard-hide-duration -float 0.0
+defaults write com.apple.dock springboard-page-duration -float 0.0
+# set dock effect to scale
 defaults write com.apple.dock mineffect -string scale
-killall Dock
+# don’t show recent applications
+defaults write com.apple.dock show-recents -bool false
+# don’t animate opening applications
+defaults write com.apple.dock launchanim -bool false
+# show indicator lights for open applications
+defaults write com.apple.dock show-process-indicators -bool true
+# don't rearange sapces
+defaults write com.apple.dock mru-spaces -bool false
+# minimize windows into their application’s icon
+defaults write com.apple.dock minimize-to-application -bool true
 
+# show hidden files
 defaults write com.apple.finder AppleShowAllFiles -bool true
+# don't show anything on the desktop
+defaults write com.apple.finder CreateDesktop -bool false
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
+# reduce animations
 defaults write com.apple.finder DisableAllAnimations -bool true
+# don't ask when changing file extensions
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+# set colunm view in Finder
 defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
-killall Finder
+# show pathbar
+defaults write com.apple.finder ShowPathbar -bool true
+# sort folder first
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+# hide status bar (bottom of Finder window)
+defaults write com.apple.finder ShowStatusBar -bool false
 
+# never open downloaded stuff automatically
 defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+# show the developer menu in Safari
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
+# show full url
+defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
+
+# don't flash the min/sec seperator in menubar
+defaults write com.apple.menuextra.clock FlashDateSeparators -bool false
+
+# disable notification on new song
+defaults write com.apple.Music userWantsPlaybackNotifications -bool false
+
+# kill processes to update settings
+killall Dock
+killall Finder
+killall SystemUIServer
+killall Safari
+killall Music
 
 # All done!
 echo "---"
